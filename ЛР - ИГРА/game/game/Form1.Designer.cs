2072b0c35931d12384912aba221b30ef05a92c91
v4d1mka <donec009@gmail.com>
2024-05-25 14:14:47 +0600

    Add files via upload

---
+++ ЛР - ИГРА/game/game/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿
+namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.img1 = new System.Windows.Forms.Label();
+            this.img2 = new System.Windows.Forms.Label();
+            this.img3 = new System.Windows.Forms.Label();
+            this.img4 = new System.Windows.Forms.Label();
+            this.img5 = new System.Windows.Forms.Label();
+            this.img6 = new System.Windows.Forms.Label();
+            this.img7 = new System.Windows.Forms.Label();
+            this.img8 = new System.Windows.Forms.Label();
+            this.img9 = new System.Windows.Forms.Label();
+            this.img10 = new System.Windows.Forms.Label();
+            this.img11 = new System.Windows.Forms.Label();
+            this.img12 = new System.Windows.Forms.Label();
+            this.img13 = new System.Windows.Forms.Label();
+            this.img14 = new System.Windows.Forms.Label();
+            this.img15 = new System.Windows.Forms.Label();
+            this.img16 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.img16, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.img15, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.img14, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.img13, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.img12, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.img11, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.img10, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.img9, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.img8, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.img7, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.img6, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.img5, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.img4, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.img3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.img2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.img1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 127F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 117F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(534, 511);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // img1
+            // 
+            this.img1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img1.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img1.Location = new System.Drawing.Point(5, 2);
+            this.img1.Name = "img1";
+            this.img1.Size = new System.Drawing.Size(125, 128);
+            this.img1.TabIndex = 0;
+            this.img1.Text = "c";
+            this.img1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img1.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img2
+            // 
+            this.img2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img2.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img2.Location = new System.Drawing.Point(138, 2);
+            this.img2.Name = "img2";
+            this.img2.Size = new System.Drawing.Size(125, 128);
+            this.img2.TabIndex = 1;
+            this.img2.Text = "c";
+            this.img2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img2.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img3
+            // 
+            this.img3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img3.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img3.Location = new System.Drawing.Point(271, 2);
+            this.img3.Name = "img3";
+            this.img3.Size = new System.Drawing.Size(125, 128);
+            this.img3.TabIndex = 2;
+            this.img3.Text = "c";
+            this.img3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img3.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img4
+            // 
+            this.img4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img4.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img4.Location = new System.Drawing.Point(404, 2);
+            this.img4.Name = "img4";
+            this.img4.Size = new System.Drawing.Size(125, 128);
+            this.img4.TabIndex = 3;
+            this.img4.Text = "c";
+            this.img4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img4.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img5
+            // 
+            this.img5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img5.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img5.Location = new System.Drawing.Point(5, 132);
+            this.img5.Name = "img5";
+            this.img5.Size = new System.Drawing.Size(125, 128);
+            this.img5.TabIndex = 4;
+            this.img5.Text = "c";
+            this.img5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img5.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img6
+            // 
+            this.img6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img6.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img6.Location = new System.Drawing.Point(138, 132);
+            this.img6.Name = "img6";
+            this.img6.Size = new System.Drawing.Size(125, 128);
+            this.img6.TabIndex = 5;
+            this.img6.Text = "c";
+            this.img6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img6.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img7
+            // 
+            this.img7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img7.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img7.Location = new System.Drawing.Point(271, 132);
+            this.img7.Name = "img7";
+            this.img7.Size = new System.Drawing.Size(125, 128);
+            this.img7.TabIndex = 6;
+            this.img7.Text = "c";
+            this.img7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img7.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img8
+            // 
+            this.img8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img8.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img8.Location = new System.Drawing.Point(404, 132);
+            this.img8.Name = "img8";
+            this.img8.Size = new System.Drawing.Size(125, 128);
+            this.img8.TabIndex = 7;
+            this.img8.Text = "c";
+            this.img8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img8.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img9
+            // 
+            this.img9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img9.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img9.Location = new System.Drawing.Point(5, 262);
+            this.img9.Name = "img9";
+            this.img9.Size = new System.Drawing.Size(125, 127);
+            this.img9.TabIndex = 8;
+            this.img9.Text = "c";
+            this.img9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img9.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img10
+            // 
+            this.img10.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img10.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img10.Location = new System.Drawing.Point(138, 262);
+            this.img10.Name = "img10";
+            this.img10.Size = new System.Drawing.Size(125, 127);
+            this.img10.TabIndex = 9;
+            this.img10.Text = "c";
+            this.img10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img10.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img11
+            // 
+            this.img11.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img11.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img11.Location = new System.Drawing.Point(271, 262);
+            this.img11.Name = "img11";
+            this.img11.Size = new System.Drawing.Size(125, 127);
+            this.img11.TabIndex = 10;
+            this.img11.Text = "c";
+            this.img11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img11.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img12
+            // 
+            this.img12.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img12.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img12.Location = new System.Drawing.Point(404, 262);
+            this.img12.Name = "img12";
+            this.img12.Size = new System.Drawing.Size(125, 127);
+            this.img12.TabIndex = 11;
+            this.img12.Text = "c";
+            this.img12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img12.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img13
+            // 
+            this.img13.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img13.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img13.Location = new System.Drawing.Point(5, 391);
+            this.img13.Name = "img13";
+            this.img13.Size = new System.Drawing.Size(125, 118);
+            this.img13.TabIndex = 12;
+            this.img13.Text = "c";
+            this.img13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img13.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img14
+            // 
+            this.img14.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img14.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img14.Location = new System.Drawing.Point(138, 391);
+            this.img14.Name = "img14";
+            this.img14.Size = new System.Drawing.Size(125, 118);
+            this.img14.TabIndex = 13;
+            this.img14.Text = "c";
+            this.img14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img14.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img15
+            // 
+            this.img15.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img15.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img15.Location = new System.Drawing.Point(271, 391);
+            this.img15.Name = "img15";
+            this.img15.Size = new System.Drawing.Size(125, 118);
+            this.img15.TabIndex = 14;
+            this.img15.Text = "c";
+            this.img15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img15.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // img16
+            // 
+            this.img16.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.img16.Font = new System.Drawing.Font("Webdings", 48F);
+            this.img16.Location = new System.Drawing.Point(404, 391);
+            this.img16.Name = "img16";
+            this.img16.Size = new System.Drawing.Size(125, 118);
+            this.img16.TabIndex = 15;
+            this.img16.Text = "c";
+            this.img16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.img16.Click += new System.EventHandler(this.clickbtn);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 750;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Matching Game";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label img16;
+        private System.Windows.Forms.Label img15;
+        private System.Windows.Forms.Label img14;
+        private System.Windows.Forms.Label img13;
+        private System.Windows.Forms.Label img12;
+        private System.Windows.Forms.Label img11;
+        private System.Windows.Forms.Label img10;
+        private System.Windows.Forms.Label img9;
+        private System.Windows.Forms.Label img8;
+        private System.Windows.Forms.Label img7;
+        private System.Windows.Forms.Label img6;
+        private System.Windows.Forms.Label img5;
+        private System.Windows.Forms.Label img4;
+        private System.Windows.Forms.Label img3;
+        private System.Windows.Forms.Label img2;
+        private System.Windows.Forms.Label img1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ЛР - ИГРА/game/game/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        Label firstClicked = null;
+        Label secondClicked = null;
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+            
+        }
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "o", "q", "в", "p", "u", "i", "r", "b", "o", "q", "в", "p", "u", "i", "r", "b"
+        };
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    icons.RemoveAt(randomNumber);
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                }
+            }
+        }
+
+        private void clickbtn(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (secondClicked != null)
+                return;
+
+            if (clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+                CheckForWinner();
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
+                timer1.Start();
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+        }
+        private void CheckForWinner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+            MessageBox.Show("Вы собрали все картинки!", "Поздравляем");
+            Close();
+        }
+
+    }
+}
